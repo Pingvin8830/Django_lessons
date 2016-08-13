b079d106e820e33e64d7c8f08f89b5379d8e147c
Vlasov Anton <Vlasov-Ant@yandex.ru>
2016-08-13 14:29:53 +0300

    Исправление ошибки

--- djbook/mysite/polls/views.py
+++ djbook/mysite/polls/views.py
@@ -18,7 +18,7 @@ def index_old (request):
 def index (request):
 	latest_question_list = Question.objects.order_by ('-pub_date') [:5]
 	context              = {
-		'latest_question_list': latest_auestion_list,
+		'latest_question_list': latest_question_list,
 	}
 	return render (request, 'polls/index.html', context)
 
