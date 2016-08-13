9f99383876f9be06f9eb7e49ba674a8f8233cd63
Vlasov Anton <Vlasov-Ant@yandex.ru>
2016-08-13 14:43:30 +0300

    Перехват исключения 404 другим способом

--- djbook/mysite/polls/views.py
+++ djbook/mysite/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http      import HttpResponse, Http404
+from django.shortcuts import render,         get_object_or_404
+from django.http      import HttpResponse,   Http404
 from django.template  import RequestContext, loader
 from .models          import Question
 
@@ -22,11 +22,15 @@ def index (request):
 	}
 	return render (request, 'polls/index.html', context)
 
-def detail (request, question_id):
+def detail_old (request, question_id):
 	try:
 		question = Question.objects.get (pk = question_id)
 	except Question.DoesNotExist:
 		raise Http404 ('Question does not exist.')
+	return render (request, 'polls/detail.html', {'question': question})
+
+def detail (request, question_id):
+	question = get_object_or_404 (Question, pk = question_id)
 	return render (request, 'polls/detail.html', {'question': question})
 
 def results (request, question_id):
