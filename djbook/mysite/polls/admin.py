23a3f7a1225c06e0cb748e0810976df664960c00
Vlasov Anton <Vlasov-Ant@yandex.ru>
2016-08-15 09:48:25 +0300

    Добавление ответов в форму добавления вопросов интерфейса администратора

--- djbook/mysite/polls/admin.py
+++ djbook/mysite/polls/admin.py
@@ -3,12 +3,18 @@ from .models        import Question, Choice
 
 # Register your models here.
 # admin.site.register (Question)
+# admin.site.register (Choice)
+
+class ChoiceInline (admin.StackedInline):
+	model = Choice
+	extra = 3
 
 class QuestionAdmin (admin.ModelAdmin):
 	fieldsets = [
 		(None,               {'fields': ['question_text']}),
 		('Date information', {'fields': ['pub_date']}),
 	]
+	inlines = [ChoiceInline]
 
 class QuestionAdmin_old (admin.ModelAdmin):
 	fields = [
@@ -17,4 +23,3 @@ class QuestionAdmin_old (admin.ModelAdmin):
 	]
 
 admin.site.register (Question, QuestionAdmin)
-admin.site.register (Choice)
